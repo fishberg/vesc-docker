3d5dea23a31af4c31f050fdc37cf0e97557d1e5b
Andrew Fishberg <fishberg.dev@gmail.com>
2023-04-26 19:24:48 -0400

    bridged /dev folder

--- run.bash
+++ run.bash
@@ -1,6 +1,6 @@
 #/usr/bin/env bash
 #docker run -it vesc-docker
 
-DEVICES="--device=/dev/input/js0 --device=/dev/ttyVESC"
+DEVICES="--device=/dev/input/js0 --device=/dev/ttyVESC -v /dev:/dev"
 
 docker run -it --net=host --privileged $DEVICES -v $(pwd)/bridge:/root/bridge vesc-docker
